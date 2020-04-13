ab1be71b2e36f7e285f2647f43e0f8210860bbfe
Oj <oj@oojmed.com>
2020-04-13 21:10:23 +0100

    Fix #D

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function transpileScript(origContent) {
   ];
 
   const conversionValues = [
-    'logging.log($1)',
+    'global.logging.log($1)',
     'emumudInternals.runner.runScriptName(\'$2\', emumud_context, undefined)',
     'emumudInternals.runner.runScriptName(\'$2\', emumud_context, $3)',
   ];
